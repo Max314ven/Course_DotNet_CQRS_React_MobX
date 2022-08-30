fec68b8ae63e565503d6e261fd5bf4a50b861d9f
mpiven <mpiven@sii.pl>
2022-08-30 15:46:50 +0200

    Connected CRUD operations with Activities with backend

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,14 +14,15 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 
 }
 
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}/>
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting} />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -34,7 +35,8 @@ export default function ActivityDashboard({ activities, selectedActivity, select
                     <ActivityForm
                         activity={selectedActivity}
                         closeForm={closeForm}
-                        createOrEdit={createOrEdit} />
+                        createOrEdit={createOrEdit}
+                        submitting={submitting} />
                 }
 
             </Grid.Column>
